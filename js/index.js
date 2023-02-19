1f1a5add82753e50ae0c7d69e5abfbced91529bc
fvaldes0109 <fvaldes0109@gmail.com>
2023-02-19 00:21:06 -0500

    Geolocation query

--- js/index.js
+++ js/index.js
@@ -1,24 +1,44 @@
 var targetElements = [];
+var input;
 
 const init = () => {
 
     targetElements = document.querySelectorAll('.info-value');
+    input = document.querySelector('input');
+    input.addEventListener('keypress', (event) => {
+        
+        if (event.key == 'Enter') {
+            event.preventDefault();
+            document.querySelector('button').click();
+        }
+    })
 
     getGeolocation();
 }
 
-const getGeolocation = async (query = '') => {
+const getGeolocation = async (fromInput) => {
 
+
+    var query = '';
     var url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_RPwwuRa3at32ZKgoegEmoWb4FBBQl`;
 
-    // Default values
-    var ip = '192.212.174.101';
-    targetElements[0].innerHTML = '192.212.174.101';
-    targetElements[1].innerHTML = 'Brooklyn, NY 10001';
-    targetElements[2].innerHTML = 'UTC -05;00';
-    targetElements[3].innerHTML = 'SpaceX Starlink';
+    var ip;
 
-    try {        
+    if (fromInput) {
+        if (input.value === '') return;
+        query = input.value;
+    }
+    input.value = '';
+
+    // Use regex to validate if 'query' is an ipv4 or ipv6
+    if (query.match(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/) || query.match(/^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/)) {
+        url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_RPwwuRa3at32ZKgoegEmoWb4FBBQl&ipAddress=${query}`;
+    }
+    else url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_RPwwuRa3at32ZKgoegEmoWb4FBBQl&domain=${query}`;
+
+    try {
+        // throw new Error('Dummy error');
+
         var response = await fetch(url);
         var data = await response.json();
 
@@ -31,6 +51,13 @@ const getGeolocation = async (query = '') => {
         
     } catch (error) {
         console.log(error);
+        
+        // Default values
+        ip = '192.212.174.101';
+        targetElements[0].innerHTML = 'Error fetching data';
+        targetElements[1].innerHTML = 'Error fetching data';
+        targetElements[2].innerHTML = 'Error fetching data';
+        targetElements[3].innerHTML = 'Error fetching data';
     }
     
 }
